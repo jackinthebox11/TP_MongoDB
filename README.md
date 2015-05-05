b52e1521cc54f88f0f367853105a1d7932fd84f7
aberteaux <alexandre.berteaux@niji.fr>
2015-05-05 17:51:01 +0200

    reponse question

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TP_MongoDB
+
+##Quelles sont les limites d’une base données orientée documents ?
+
+###Definition :
+C'est un type de base de données destinée à stocker des documents. Assez proche du modèle des bases SQL, les bases de documents n'en offrent pas moins une souplesse bien plus grande. Certains documents d'une collection peuvent avoir des champs supplémentaires.
+En outre, le contenu d'un document ne se limite pas à des attributs simples, on peut également avoir des tableaux, voire inclure un autre document dans celui-ci. Par exemple, il est possible de stocker les commentaires liés à un article directement dans celui-ci, sous forme d'un tableau d'objets dépendants.
+
+###Limites :
+
+* Les bases NoSQL manquent de standardisation et de normalisation.
+
+* Les bases NoSQL sont généralement assez limitées au niveau du support des transactions. Si l'édition d'un objet se fait en principe de manière atomique, dès qu'il s'agit de modifier plusieurs objets dépendant les uns des autres, la base n'offre plus les garanties qu'on retrouve avec le relationnel.
+
+* Les contraintes d'intégrité ne sont généralement pas implémentées par les bases de données NoSQL, cela délègue cette gestion à la partie applicative.
+
+* Les bases SQL sont souvent plus performantes sur les requêtes complexes qui nécessiteraient de rapatrier plus de données que nécessaire pour les traiter en NoSQL.
--- src/main/java/fr/istic/tpMongoD/App.java
+++ src/main/java/fr/istic/tpMongoD/App.java
@@ -1,6 +1,8 @@
 package fr.istic.tpMongoD;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
@@ -21,18 +23,79 @@ public class App
     	
     	Person p = new Person();
     	p.setName("Tintin");
+    	
+    	Person p1 = new Person();
+    	p1.setName("Alex");
+    	
+    	Person p2 = new Person();
+    	p2.setName("Thibault");
+    	
+        ds.save(p);
+        ds.save(p1);
+        ds.save(p2);
 
     	Address address = new Address();
     	address.setStreet("123 Some street");
     	address.setCity("Some city");
     	address.setPostCode("123 456");
     	address.setCountry("Some country");
+    	
+    	Address address1 = new Address();
+    	address1.setStreet("6 rue perrin de la touche");
+    	address1.setCity("Rennes");
+    	address1.setPostCode("35000");
+    	address1.setCountry("France");
+    	
+    	Address address2 = new Address();
+    	address2.setStreet("24 rue du burel");
+    	address2.setCity("Rennes");
+    	address2.setPostCode("35000");
+    	address2.setCountry("France");
+    	
     	//set address
-    	//p.setAddress(address);
+    	List<Address> ad = new ArrayList<Address>();
+		ad.add(address);
+		p.setAddress(ad);
+		
+    	List<Address> ad1 = new ArrayList<Address>();
+		ad1.add(address1);
+		p1.setAddress(ad1);
+		
+    	List<Address> ad2 = new ArrayList<Address>();
+		ad2.add(address2);
+		p2.setAddress(ad2);
+		
     	// Save the POJO
     	ds.save(address);
+    	ds.save(address1);
+    	ds.save(address2);
     	
     	
+    	Article article = new Article();
+        article.setName("mon article");
+        
+        Article article1 = new Article();
+        article1.setName("console");
+        
+        Article article2 = new Article();
+        article2.setName("lit");
+        
+        ArrayList<Person> buyer = new ArrayList<Person>();
+        buyer.add(p);
+        
+        ArrayList<Person> buyer1 = new ArrayList<Person>();
+        buyer1.add(p1);
+        
+        ArrayList<Person> buyer2 = new ArrayList<Person>();
+        buyer2.add(p2);
+
+        article.setBuyers(buyer);
+        article1.setBuyers(buyer1);
+        article2.setBuyers(buyer2);
+    	
+        ds.save(article);
+        ds.save(article1);
+        ds.save(article2);
     	
     	for (Person e : ds.find(Person.class))
     		 System.err.println(e);
